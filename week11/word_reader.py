a95765903ab80d0825a872e7334ab121a26036b2
Kirill Setdekov <ksetdekov@gmail.com>
2020-03-29 14:55:28 +0300

    book ex 1 done

---
+++ week11/word_reader.py
@@ -0,0 +1,14 @@
+name = "../files/words.txt"
+handle = open(name)
+
+counts = dict()
+i = 0
+for line in handle:
+    words = line.split()
+    for word in words:
+        counts[word] = counts.get(word, i)
+        i = i + 1
+handle.close()
+print(counts)
+
+print('tasks' in counts)
