42c932cc118c0f3cf5ab0ca0f215aa631446e325
ksetdekov <Qe82SPsYawm2vwFpHrCc>
2020-04-02 12:04:13 +0300

    started tuples ex1

---
+++ week12/ex10_.py
@@ -0,0 +1,23 @@
+name = input("Enter file:")
+if len(name) < 1:
+    name = "../files/mbox-short.txt"
+
+fh = open(name)
+count = 0
+counts = dict()
+for line in fh:
+    if not line.startswith("From "):
+        continue
+    line_words = line.rstrip().split()
+    if line_words[0] == 'From':
+        sender = (line_words[1])
+        counts[sender] = counts.get(sender, 0) + 1
+
+fh.close()
+highest = None
+bigcount = None
+for k, v in counts.items():
+    if highest is None or v > bigcount:
+        highest = k
+        bigcount = v
+print(highest, counts[highest])
