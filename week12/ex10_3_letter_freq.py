0bdec8e57b611420d563252c7fba2ebc09032b6d
ksetdekov <Qe82SPsYawm2vwFpHrCc>
2020-04-03 10:07:35 +0300

    done ex10_3_letter_freq.py

---
+++ week12/ex10_3_letter_freq.py
@@ -0,0 +1,20 @@
+import string
+
+name = input("Enter file:")
+if len(name) < 1:
+    name = '../files/romeo-full.txt'
+
+ValidLetters = string.ascii_letters[:26]
+i = 0
+fhand = open('../files/romeo-full.txt')
+counts = dict()
+for line in fhand:
+    line = line.rstrip()
+    line = line.translate(line.maketrans('', '', string.punctuation))
+    line = line.lower()
+    for letter in line:
+        if letter in ValidLetters:
+            counts[letter] = counts.get(letter, 0) + 1
+# list comprehension
+fhand.close()
+print(sorted([(v, k) for k, v in counts.items()], reverse=True)[:10])
