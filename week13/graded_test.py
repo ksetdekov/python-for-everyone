105dcd0e4efe83e9e8efc8752be357d60082e53c
Kirill Setdekov <ksetdekov@gmail.com>
2020-04-05 16:31:09 +0300

    did in 1 line

--- week13/graded_test.py
+++ week13/graded_test.py
@@ -1,13 +1,3 @@
 import re
-fhand = open('regex_sum_382339.txt')
-numbers = list()
-for line in fhand:
-    linenumbers = re.findall('[0-9]+', line)
-    if len(linenumbers) == 0:
-        continue
-    for num in linenumbers:
-        num = int(num)
-        numbers.append(num)
-# list comprehension
-fhand.close()
-print(sum(numbers))
+
+print(sum([int(x) for x in re.findall('[0-9]+', open('regex_sum_382339.txt').read())]))
