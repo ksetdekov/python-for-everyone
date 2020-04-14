745dc29328df7005e0405d06e89516eb06c41e46
ksetdekov <Qe82SPsYawm2vwFpHrCc>
2020-04-14 15:32:45 +0300

    worked example done

--- week15/urllinks.py
+++ week15/urllinks.py
@@ -1,5 +1,12 @@
+import ssl
 import urllib.request
+
 from bs4 import BeautifulSoup
+
+# ignore ssl certificate error
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
 url = input('Enter url')
 html = urllib.request.urlopen(url).read()
@@ -9,4 +16,3 @@ soup = BeautifulSoup(html, 'html.parser')
 tags = soup('a')
 for tag in tags:
     print(tag.get('href', None))
-    print(tag)
