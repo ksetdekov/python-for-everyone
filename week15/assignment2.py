3a408cf7c3f57c5d2f8350ee8e75224c555f56ea
ksetdekov <Qe82SPsYawm2vwFpHrCc>
2020-04-14 16:55:09 +0300

    finished assignment2.py

--- week15/assignment2.py
+++ week15/assignment2.py
@@ -8,17 +8,24 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url = 'http://py4e-data.dr-chuck.net/known_by_Fikret.html'
+url = 'http://py4e-data.dr-chuck.net/known_by_Amnah.html'
 count = int(input('Enter count:'))
 pos = int(input('Enter position:'))
-
-html = urllib.request.urlopen(url).read()
-soup = BeautifulSoup(html, 'html.parser')
-
-#  get anchor tags
-tags = soup('a')
-# for tag in tags:
-# print(tag.get('href', None))
-result = tags[pos - 1].get('href', None)
 print(url)
-print(result)
+
+
+def fall(link, n):
+    html = urllib.request.urlopen(link).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    #  get anchor tags
+    tags = soup('a')
+    # for tag in tags:
+    # print(tag.get('href', None))
+    result = tags[n - 1].get('href', None)
+    print(result)
+    return result
+
+
+while count > 0:
+    url = fall(url, pos)
+    count += -1
