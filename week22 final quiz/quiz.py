e7a8579e7ef947d8d237a20d86d6ec48881e6c22
Kirill Setdekov <ksetdekov@gmail.com>
2020-05-03 14:06:58 +0300

    finished

---
+++ week22 final quiz/quiz.py
@@ -0,0 +1,136 @@
+# 1
+print('utf-8')
+# 2
+print(ord('G'))
+# 3
+words = [108, 105, 115, 116]
+for i in words:
+    print(chr(i))
+# 4
+print('unicode')
+# 5
+print('decode')
+# 6
+x = -19
+if x < 2:
+    print("Below 2")
+elif x < 0:
+    print("Negative")
+else:
+    print("Something else")
+
+print('Negative')
+
+# 7
+x = 8
+
+if x < 2:
+    print("Below 2")
+elif x < 20:
+    print("Below 20")
+elif x < 10:
+    print("Below 10")
+else:
+    print("Something else")
+
+print('Below 10')
+
+# 8
+zap = "hello there bob"
+print(zap[4])
+
+# 9
+x = 12
+if x < 5:
+    print("smaller")
+else:
+    print("bigger")
+print("all done")
+print(3)
+
+
+# 10
+def fred():
+    print("Zap")
+
+
+def jane():
+    print("ABC")
+
+
+jane()
+fred()
+jane()
+
+# 11
+print('main memory')
+
+# 12
+print('key-value pairs')
+
+# 13
+print(42)
+
+# 14
+stuff = ['joseph', 'sally', 'walter', 'tim']
+print(stuff[2])
+
+
+# 15
+def hello():
+    print("Hello")
+    print("There")
+
+
+x = 10
+x = x + 1
+
+# 16
+x = -1
+for value in [3, 41, 12, 9, 74, 15]:
+    if value > x:
+        x = value
+print(x)
+
+# 17
+total = 0
+for abc in range(5):
+    total = total + abc
+print(total)
+
+# 18
+# a = "123"
+# b = 456
+# c = a + b
+# print(c)
+print('line', 3)
+
+# 19
+print(1, 2, 3, 5, 6)
+
+# 20
+abc = "With three words"
+stuff = abc.split()
+print(stuff)
+
+# 21
+abc = "With three words"
+stuff = abc.split()
+print(len(stuff))
+
+# 22
+print('direction')
+
+# 23
+print('refer to the instance in which the method is called')
+
+# 24
+print('read and write with same')
+
+# 25 ??
+print('complex element')
+print('simple element')
+print('attribute')
+
+# 26
+print('js')
